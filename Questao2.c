52b36d01ce7efd714df21a0ade3e00b628b1404c
Samuel Iago <146142470+samuelIago@users.noreply.github.com>
2023-11-29 16:57:27 +0000

    resposta 2

--- Questao2.c
+++ Questao2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main (void){
     printf("O seguinte programa cria uma matriz 3X3, e realiza as operações de soma e subtração que estão presentes, utilizando os valores correspodentes dentro da matriz\n");
+    printf("0-4+0-1-0-0=-5");
     printf("O valor final é -5\n");
 }
